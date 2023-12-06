c130695ffc728fa239df25b9662ebe472d05f418
Voronova Diana <i@dinavoron.ru>
2023-12-06 17:50:36 +1000

    Оставить только быстрые методы

--- parallel_prog/main.cpp
+++ parallel_prog/main.cpp
@@ -332,178 +332,72 @@ double average_cpp_reduction(const double* v, size_t N) {
 
 
 int main() {
-	std::size_t N = 1u << 25;
+	std::size_t N = 1u << 30;
 	auto buf = std::make_unique<double[]>(N);
 	for (std::size_t i = 0; i < N; ++i) buf[i] = i;
-
-
 	
-
-	/*auto t1 = omp_get_wtime();
-	auto v1 = average(buf.get(), N);
-	auto t2 = omp_get_wtime();
-	std::cout << "Result (classical): " << v1 << "\n";
-	std::cout << "Time taken (classical): " << t2 - t1 << "\n";
-	auto res = run_experiment(average, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-	/*auto t3 = omp_get_wtime();
-	auto v2 = average_reduce(buf.get(), N);
-	auto t4 = omp_get_wtime();
-	std::cout << "Result (reduce): " << v2 << "\n";
-	std::cout << "Time taken (reduce): " << t4 - t3 << "\n";
-	auto res = run_experiment(average_reduce, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_reduce)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-	/*auto t5 = omp_get_wtime();
-	auto v3 = average_rr(buf.get(), N);
-	auto t6 = omp_get_wtime();
-	std::cout << "Result (Round Robin): " << v3 << "\n";
-	std::cout << "Time taken (Round Robin): " << t6 - t5 << "\n";
-
-	res = run_experiment(average_rr, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_rr)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-
-	/*auto t7 = omp_get_wtime();
-	auto v4 = average_omp(buf.get(), N);
-	auto t8 = omp_get_wtime();
-	std::cout << "Result (Open MP): " << v4 << "\n";
-	std::cout << "Time taken (Open MP): " << t8 - t7 << "\n";
-	auto res = run_experiment(average_omp, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_omp)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-	/*auto t9 = omp_get_wtime();
-	auto v5 = average_omp_modified(buf.get(), N);
-	auto t10 = omp_get_wtime();
-	std::cout << "Result (Open MP modified): " << v5 << "\n";
-	std::cout << "Time taken (Open MP modified): " << t10 - t9 << "\n";
-	auto res = run_experiment(average_omp_modified, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_omp_modified)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-	auto t11 = omp_get_wtime();
-	auto v6 = average_omp_mtx(buf.get(), N);
-	auto t12 = omp_get_wtime();
-	std::cout << "Result (Open MP Mutex): " << v6 << "\n";
-	std::cout << "Time taken (Open MP Mutex): " << t12 - t11 << "\n";
-	auto res = run_experiment(average_omp_mtx, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_omp_mtx)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
+	{
+		auto res = run_experiment(average_reduce, buf.get(), N);
+		std::cout << "Result,Time,Speedup,Efficiency(average_reduce)" << "\n";
+		for (int i = 0; i < res.size(); i++)
+			std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
 	}
 
-	/*auto t13 = omp_get_wtime();
-	auto v7 = average_omp_mtx_modified(buf.get(), N);
-	auto t14 = omp_get_wtime();
-	std::cout << "Result (Open MP Mutex modified): " << v7 << "\n";
-	std::cout << "Time taken (Open MP Mutex modified): " << t14 - t13 << "\n";
+	
+	{
+		auto res = run_experiment(average_omp_modified, buf.get(), N);
+		std::cout << "Result,Time,Speedup,Efficiency(average_omp_modified)" << "\n";
+		for (int i = 0; i < res.size(); i++)
+			std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
+	}
+	
+	
+	{
+		auto res = run_experiment(average_omp_mtx_modified, buf.get(), N);
+		std::cout << "Result,Time,Speedup,Efficiency(average_omp_mtx_modified)" << "\n";
+		for (int i = 0; i < res.size(); i++) {
+			std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
+		}
+	}
 
-	res = run_experiment(average_omp_mtx_modified, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_omp_mtx_modified)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
+	{
+		auto res = run_experiment(average_cpp_mtx, buf.get(), N);
+		std::cout << "Result,Time,Speedup,Efficiency(average_cpp_mtx)" << "\n";
+		for (int i = 0; i < res.size(); i++) {
+			std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
+		}
+	}
 
-	/*auto t15 = omp_get_wtime();
-	auto v8 = average_cpp_mtx(buf.get(), N);
-	auto t16 = omp_get_wtime();
-	std::cout << "Result (CPP Mutex): " << v8 << "\n";
-	std::cout << "Time taken (CPP Mutex): " << t16 - t15 << "\n";
+	{
+		auto res = run_experiment(average_cpp_mtx_local, buf.get(), N);
+		std::cout << "Result,Time,Speedup,Efficiency(average_cpp_mtx_local)" << "\n";
+		for (int i = 0; i < res.size(); i++)
+			std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
+	}
 
-	res = run_experiment(average_cpp_mtx, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_cpp_mtx)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
+	{
+		auto res = run_experiment(average_cpp_reduction, buf.get(), N);
+		std::cout << "Result,Time,Speedup,Efficiency(average_cpp_reduction)" << "\n";
+		for (int i = 0; i < res.size(); i++) {
+			std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
+		}
+	}
 
-	/*auto t17 = omp_get_wtime();
-	auto v9 = average_cpp_mtx_modified1(buf.get(), N);
-	auto t18 = omp_get_wtime();
-	std::cout << "Result (CPP Mutex modified0): " << v9 << "\n";
-	std::cout << "Time taken (CPP Mutex modified0): " << t18 - t17 << "\n";
+	{
+		auto res = run_experiment(average_omp_aligned, buf.get(), N);
+		std::cout << "Result,Time,Speedup,Efficiency(average_omp_aligned)" << "\n";
+		for (int i = 0; i < res.size(); i++) {
+			std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
+		}
+	}
 
-	res = run_experiment(average_cpp_mtx_modified1, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_cpp_mtx_modified1)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
+	{
+		auto res = run_experiment(average_cpp_aligned, buf.get(), N);
+		std::cout << "Result,Time,Speedup,Efficiency(average_cpp_aligned)" << "\n";
+		for (int i = 0; i < res.size(); i++) 
+			std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
 
-	/*auto t19 = omp_get_wtime();
-	auto v10 = average_cpp_mtx_modified1(buf.get(), N);
-	auto t20 = omp_get_wtime();
-	std::cout << "Result (CPP Mutex modified1): " << v10 << "\n";
-	std::cout << "Time taken (CPP Mutex modified1): " << t20 - t19 << "\n";
-
-	res = run_experiment(average_cpp_mtx_modified1, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_cpp_mtx_modified1)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-	/*auto t21 = omp_get_wtime();
-	auto v11 = average_cpp_mtx_local(buf.get(), N);
-	auto t22 = omp_get_wtime();
-	std::cout << "Result (CPP Mutex local): " << v11 << "\n";
-	std::cout << "Time taken (CPP Mutex local): " << t22 - t21 << "\n";
-
-	res = run_experiment(average_cpp_mtx_local, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_cpp_mtx_local)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-	/*auto t23 = omp_get_wtime();
-	auto v12 = average_cpp_reduction(buf.get(), N);
-	auto t24 = omp_get_wtime();
-	std::cout << "Result (CPP reduction): " << v12 << "\n";
-	std::cout << "Time taken (CPP redaction): " << t24 - t23 << "\n";
-
-	res = run_experiment(average_cpp_reduction, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_cpp_reduction)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-
-	/*auto t25 = omp_get_wtime();
-	auto v13 = average_omp_aligned(buf.get(), N);
-	auto t26 = omp_get_wtime();
-	std::cout << "Result (OMP aligned): " << v12 << "\n";
-	std::cout << "Time taken (OMP aligned): " << t24 - t23 << "\n";
-
-	res = run_experiment(average_omp_aligned, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_omp_aligned)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
-
-
-	/*auto t27 = omp_get_wtime();
-	auto v14 = average_cpp_aligned(buf.get(), N);
-	auto t28 = omp_get_wtime();
-	std::cout << "Result (cpp aligned): " << v12 << "\n";
-	std::cout << "Time taken (cpp aligned): " << t24 - t23 << "\n";
-
-	res = run_experiment(average_cpp_aligned, buf.get(), N);
-	std::cout << "Result,Time,Speedup,Efficiency(average_cpp_aligned)" << "\n";
-	for (int i = 0; i < res.size(); i++) {
-		std::cout << res[i].result << "," << res[i].time << "," << res[i].speedup << "," << res[i].efficiency << "\n";
-	}*/
+	}
 
 	return 0;
 }
